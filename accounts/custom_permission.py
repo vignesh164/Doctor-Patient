0190c2b387e156772d04e06aaa8d8db44f4a8ed2
Vigneshwaran <38978461+vignesh164@users.noreply.github.com>
2020-03-28 18:54:53 +0530

    initial commit vignesh

---
+++ accounts/custom_permission.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsDoctor(BasePermission):
+    def has_permission(self, request, view):
+        return 'Doctor' in list(request.user.groups.values_list('name', flat=True))
+
+
+class IsPatient(BasePermission):
+    def has_permission(self, request, view):
+        return 'Patient' in list(request.user.groups.values_list('name', flat=True))
